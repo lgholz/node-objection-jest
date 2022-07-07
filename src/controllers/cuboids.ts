9ab40d4f3fd3df455fd4d9ac6c02eed7bb17f5b1
Lucas <lgholz@gmail.com>
2022-07-07 16:22:16 -0300

    fix for cuboid controller get and create

--- src/controllers/cuboids.ts
+++ src/controllers/cuboids.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import * as HttpStatus from 'http-status-codes';
 import { Id } from 'objection';
-import { Cuboid } from '../models';
+import { Bag, Cuboid } from '../models';
 
 export const list = async (req: Request, res: Response): Promise<Response> => {
   const ids = req.query.ids as Id[];
@@ -10,14 +10,34 @@ export const list = async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).json(cuboids);
 };
 
-export const get = async (req: Request, res: Response): Promise<Response> =>
-  res.sendStatus(200);
+export const get = async (req: Request, res: Response): Promise<Response> => {
+  const id: Id = req.params.id;
+  const cuboid = await Cuboid.query().findById(id).withGraphFetched('bag');
+
+  if (!cuboid) {
+    return res.sendStatus(HttpStatus.NOT_FOUND);
+  }
+
+  return res.status(200).json(cuboid);
+};
 
 export const create = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
   const { width, height, depth, bagId } = req.body;
+
+  const bag = await Bag.query().findById(bagId).withGraphFetched('cuboids');
+
+  if (!bag) {
+    return res.sendStatus(HttpStatus.NOT_FOUND);
+  }
+
+  if (bag.availableVolume < width * height * depth) {
+    return res
+      .status(HttpStatus.UNPROCESSABLE_ENTITY)
+      .json({ message: 'Insufficient capacity in bag' });
+  }
 
   const cuboid = await Cuboid.query().insert({
     width,
